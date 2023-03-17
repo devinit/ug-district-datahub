8cf2efc6bf2125b82bf38399b06ceea2c06d455f
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-17 06:37:54 +0300

    Add patterns

--- datahub/settings/base.py
+++ datahub/settings/base.py
@@ -139,7 +139,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'patterns/converted-html/assets'),
+    os.path.join(BASE_DIR, 'patterns/converted-html/assets'),
     os.path.join(PROJECT_DIR, "static"),
 ]
 
