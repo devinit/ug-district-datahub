95705a9d0039b59960de22e0a1fe6a2d2bfc49ce
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-17 06:51:46 +0300

    Add robots.txt

--- datahub/urls.py
+++ datahub/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
@@ -20,12 +22,20 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = [
+        path('robots.txt', TemplateView.as_view(template_name="robots.dev.txt", content_type='text/plain')),
+    ] + urlpatterns
+else:
+    urlpatterns = [
+        path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+    ] + urlpatterns
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
     path("", include(wagtail_urls)),
+    path('sitemap.xml', sitemap),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
