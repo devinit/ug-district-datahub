e7f7871df6836ac9a66288ed6bdd962f88a1856c
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-17 06:43:27 +0300

    Remove generate static folder

--- static/js/utils/simpleModal.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-export default function simpleModal (trigger, target, targetActive) {
-    $(trigger).on('click', function(e) {
-		e.preventDefault();
-		$(target).toggleClass(targetActive);
-	});
-}
