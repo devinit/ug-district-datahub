8bb408d4b1c90f36184deb581686346d1c9d209f
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-17 06:36:31 +0300

    Update database configs

--- datahub/settings/base.py
+++ datahub/settings/base.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = "datahub.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
