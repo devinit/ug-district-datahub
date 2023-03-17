c88477d0f2096a3d3a59a27c41140b2cdc2ccede
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-17 06:13:34 +0300

    Initial commit

---
+++ datahub/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&y^ac@&2!9+9c5=svjq@gr103g6u6ilvji*4f4j^=n__*offjj"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
