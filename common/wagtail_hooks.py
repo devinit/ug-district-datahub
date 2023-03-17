86578fc55b07dc80ddaeb7105c8455745b53d16b
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-17 07:57:42 +0300

    Add wagtail_hooks

---
+++ common/wagtail_hooks.py
@@ -0,0 +1,58 @@
+from django.templatetags.static import static
+from django.utils.html import format_html
+
+from wagtail import hooks
+from wagtail.whitelist import allow_without_attributes, attribute_rule
+
+
+@hooks.register('insert_editor_css')
+def editor_css():
+
+    # Any extra CSS for customising the page editor can be added here as well
+    return format_html('<link rel="stylesheet" href="{}">', static("css/editor.css"))
+
+
+@hooks.register('construct_whitelister_element_rules')
+def whitelister_element_rules():
+    return {
+        'span': attribute_rule({'style': True}),
+        'p': attribute_rule({'style': True}),
+        'sup': allow_without_attributes,
+        'sub': allow_without_attributes,
+        'code': allow_without_attributes,
+        'blockquote': allow_without_attributes,
+        'abbr': attribute_rule({'title': True}),
+    }
+
+
+@hooks.register('insert_editor_js')
+def editor_js():
+    return format_html(
+        """
+        <script>
+            $('body.create.model-personpage').each(function() {{
+                $(function(e) {{
+                    if ($('#id_slug').val() == '') {{
+                        $('#id_slug').val('placeholder');
+                    }}
+                }});
+            }});
+            $('body.page-editor').each(function() {{
+                $(function(e) {{
+                    $('#id_uuid').closest('.object.char_field').hide();
+                }});
+            }});
+            $(function() {{
+
+                var taxonomyList = $('ul#id_language, ul#id_event_type');
+                taxonomyList.find('label:contains("---------")')
+                    .contents()
+                    .filter(function() {{
+                        return this.nodeType == 3
+                    }}).each(function() {{
+                        this.textContent = this.textContent.replace('---------', 'None');
+                    }});
+            }});
+        </script>
+        """
+    )
