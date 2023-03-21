642add1c03c6275e410cfe3eb2e051e99d354878
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-21 15:56:11 +0300

    Replace report_utils with dashboard_tags

--- dashboard/templatetags/dashboard_tags.py
+++ dashboard/templatetags/dashboard_tags.py
@@ -102,3 +102,41 @@ def gets_row_highlights(context, pivot_page):
                 "color": highlight.row_highlight_colour or '#ffb3b3'
                 })
     return json.dumps(highlights)
+
+
+@register.simple_tag
+def get_previous_page(all_pages, label):
+    try:
+        for index, item in enumerate(all_pages):
+            if index > 0:
+                if item.label == label:
+                    return all_pages[index - 1]
+        return ''
+    except Exception:
+        return ''
+
+
+@register.simple_tag
+def get_next_page(all_pages, label):
+    try:
+        for index, item in enumerate(all_pages):
+            if index < len(all_pages) - 1:
+                if item.label == label:
+                    return all_pages[index + 1]
+
+        return ''
+    except Exception:
+        return ''
+
+
+@register.simple_tag
+def chapter_nav_slice(chapters, chapter_number=0, max_length=6):
+    try:
+        zerod = chapter_number - 1 if chapter_number else 0
+        length = len(chapters)
+        max_from_end = max(length - max_length, 0)
+        start = max_from_end if zerod > max_from_end else zerod
+        end = zerod + max_length
+        return '%s:%s' % (str(start), str(end))
+    except Exception:
+        return ''
