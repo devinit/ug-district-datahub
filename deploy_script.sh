067d9ef651f96b74d218471aff0fbb0bc19b5534
Wakibi Chris <wakibic@gmail.com>
2023-03-27 10:53:43 +0300

    Generate self signed certificates on first run

--- deploy_script.sh
+++ deploy_script.sh
@@ -4,50 +4,25 @@
 APP_NAME="ug-district-datahub"
 ORG_NAME="devinit"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-APP_DIR=$SCRIPT_DIR'/'$APP_NAME
-SSL_SUB_DIR="config/nginx"
+APP_DIR=$SCRIPT_DIR
 CERTBOT_SUB_DIR="ssl"
-FULL_CHAIN_LOCATION=$APP_DIR'/'$SSL_SUB_DIR'/fullchain.pem'
-PRIV_KEY_LOCATION=$APP_DIR'/'$SSL_SUB_DIR'/privkey.pem'
+OPENSSL_CONFIG=$APP_DIR'/config/openssl/ssl.conf'
 FULL_CHAIN_DESTINATION=$APP_DIR'/'$CERTBOT_SUB_DIR'/fullchain.pem'
 PRIV_KEY_DESTINATION=$APP_DIR'/'$CERTBOT_SUB_DIR'/privkey.pem'
 REPOSITORY="https://github.com/"$ORG_NAME"/"$APP_NAME".git"
 FIRST_RUN="0"
 
 
-echo "clone current branch if not exists"
-
 if [ -d $APP_DIR ]; then
       cd $APP_DIR
-  {
-      echo "Cloning new content from develop"
-      git fetch
-      git stash
-      git checkout develop
-      git reset --hard origin/dev
-      } || {
-      echo "Failed to update from git repository"
-      exit 20;
-  }
-else
-  {
-      git clone -b develop $REPOSITORY
-
-      } || {
-      echo "Failed to perform git clone on $REPOSITORY with branch dev"
-      exit 20;
-  }
 fi
 
 if [ ! -f $FULL_CHAIN_DESTINATION ]; then
     $FIRST_RUN='1'
-    cp $FULL_CHAIN_LOCATION $FULL_CHAIN_DESTINATION
+    mkdir -d $APP_DIR'/'$CERTBOT_SUB_DIR
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $PRIV_KEY_DESTINATION -out $FULL_CHAIN_DESTINATION -config $OPENSSL_CONFIG
 fi
 
-if [ ! -f $PRIV_KEY_LOCATION ]; then
-    $FIRST_RUN='1'
-    cp $PRIV_KEY_LOCATION $PRIV_KEY_DESTINATION
-fi
 
 cd ~/git/ug-district-datahub || exit
 
