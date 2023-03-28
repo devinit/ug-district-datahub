c363af2806f43b7d9baf3814adacf285d051ac85
Chris Wakibi <wakibi@users.noreply.github.com>
2023-03-28 13:35:28 +0300

    Feature/GitHub ci (#4)
    * Add docker and nginx configs
    
    * Add development compose
    
    * Add nginx and certbot configs
    
    * Add context to web container
    
    * Add command to upgrade pip
    
    * Update docker configs
    
    * Fix docker configs
    
    * Use port 8000 for web
    
    * Fix working directory
    
    * Add script to run installation
    
    * Generate self signed certificates on first run
    
    * Run openssl non-interactively
    
    * Fix variable name
    
    * Fix command option
    
    * Fix ssl certificate paths
    
    * Add step to collect static files
    
    * Add npm installation
    
    * Move npm commands to host machine
    
    * Add missing static directory
    
    * Enable github actions
    
    * Enable actions on debugging branch
    
    * Update name
    
    ---------
    
    Co-authored-by: Edwin P. Magezi <edwinm_p@yahoo.com>

---
+++ deploy_script.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+## gets run on host server
+
+APP_NAME="ug-district-datahub"
+ORG_NAME="devinit"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+APP_DIR=$SCRIPT_DIR
+CERTBOT_SUB_DIR="ssl"
+DOMAIN="live/datahub.go.ug/"
+FULL_CHAIN_DESTINATION=$APP_DIR'/'$CERTBOT_SUB_DIR'/'$DOMAIN'/fullchain.pem'
+PRIV_KEY_DESTINATION=$APP_DIR'/'$CERTBOT_SUB_DIR'/'$DOMAIN'/privkey.pem'
+REPOSITORY="https://github.com/"$ORG_NAME"/"$APP_NAME".git"
+FIRST_RUN="0"
+
+
+if [ -d $APP_DIR ]; then
+      cd $APP_DIR
+fi
+
+if [ ! -f $FULL_CHAIN_DESTINATION ]; then
+    FIRST_RUN='1'
+    mkdir -p $APP_DIR'/'$CERTBOT_SUB_DIR
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $PRIV_KEY_DESTINATION -out $FULL_CHAIN_DESTINATION -subj "/C=UG/ST=Kampala/L=Kampala/O=Global Security/OU=Devs/CN=datahub.go.ug"
+fi
+
+
+cd ~/git/ug-district-datahub || exit
+
+npm install
+npm run build
+
+echo "Building docker"
+
+docker-compose build db
+docker-compose build --no-cache web
+docker-compose build nginx
+docker-compose build certbot
+
+docker-compose down --remove-orphans
+docker-compose up -d
+
+docker-compose exec -T web python manage.py migrate --noinput
+docker-compose exec -T web python manage.py collectstatic --noinput
+
+if [ $FIRST_RUN == "1" ]; then
+    docker-compose restart web
+fi
