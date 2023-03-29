68c323161b568b3a23205681288dd06e1ec56f87
Wakibi Chris <wakibic@gmail.com>
2023-03-29 09:31:45 +0300

    Enable https only for production

--- datahub/settings/production.py
+++ datahub/settings/production.py
@@ -2,6 +2,10 @@ from .base import *
 
 DEBUG = False
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SECURE_SSL_REDIRECT = not DEBUG
+
 try:
     from .local import *
 except ImportError:
