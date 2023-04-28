b36f3d6d658e9757e157655a285c692bc4bb8992
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-04-28 21:19:56 +0300

    Update allowed hosts

--- datahub/settings/production.py
+++ datahub/settings/production.py
@@ -6,7 +6,7 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 SECURE_SSL_REDIRECT = not DEBUG
 
-ALLOWED_HOSTS = [".datahub.go.ug", "localhost", "127.0.0.1", "64.226.108.95"]
+ALLOWED_HOSTS = [".datahub.go.ug", "localhost", "127.0.0.1", "167.99.87.62", "138.68.164.192"]
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", None)
 
