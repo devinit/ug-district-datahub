c8e72b83d484329871c34ef4f44affa22805735e
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-04-28 15:27:36 +0300

    Update domain to point to Masindi

--- init_letsencrypt.sh
+++ init_letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(kayunga.datahub.go.ug www.kayunga.datahub.go.ug)
+domains=(masindi.datahub.go.ug www.masindi.datahub.go.ug)
 rsa_key_size=4096
 data_path="./certbot"
 email="emagezi@gmail.com" # Adding a valid address is strongly recommended
