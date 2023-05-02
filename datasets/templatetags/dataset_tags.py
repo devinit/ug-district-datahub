756cb09ab51fb7ede08eb74a3267b9dab02cbb8f
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-02 18:09:11 +0300

    Add link to dataset listing page

--- datasets/templatetags/dataset_tags.py
+++ datasets/templatetags/dataset_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from datasets.models import DatasetPage
+from datasets.models import DatasetPage, DatasetListing
 
 register = template.Library()
 
@@ -8,3 +8,8 @@ register = template.Library()
 @register.simple_tag(takes_context=False)
 def latest_datasets(count=6):
     return DatasetPage.objects.live().order_by("-release_date")[0:count]
+
+
+@register.simple_tag(takes_context=False)
+def dataset_listing():
+    return DatasetListing.objects.live().first()
